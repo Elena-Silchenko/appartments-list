aa9a058fe7db22f8edbdf54e1b7ce3a8fde3a3bd
Elena Silchenko <elen.silchenko@gmail.com>
2022-02-03 20:44:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Table } from 'react-bootstrap'
+import './App.css'
+import Data from './data'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="d-flex">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Rooms</th>
+            <th>Area</th>
+            <th>Floor</th>
+            <th>Parking</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Data.map((e, index) => (
+              <tr key={index}>
+                <td>{e.title}</td>
+                <td>{e.rooms}</td>
+                <td>{e.area}</td>
+                <td>{e.floor}</td>
+                <td>{e.parking ? 'Yes' : 'No'}</td>
+                <td>{e.price.toLocaleString()} zł</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
+    </div> 
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,60 @@
+const Appartments = [
+  {
+    title: 'm.1',
+    rooms: 1,
+    area: 32.3,
+    floor: 0,
+    parking: false,
+    price: 379889
+  },
+  {
+    title: 'm.2',
+    rooms: 2,
+    area: 42.7,
+    floor: 2,
+    parking: true,
+    price: 437000
+  },
+  {
+    title: 'm.3',
+    rooms: 2,
+    area: 44.5,
+    floor: 1,
+    parking: true,
+    price: 436890
+  },
+  {
+    title: 'm.4',
+    rooms: 3,
+    area: 57.4,
+    floor: 3,
+    parking: false,
+    price: 565200
+  },
+  {
+    title: 'm.5',
+    rooms: 2,
+    area: 49.7,
+    floor: 4,
+    parking: true,
+    price: 457000
+  },
+  {
+    title: 'm.6',
+    rooms: 3,
+    area: 73.2,
+    floor: 4,
+    parking: false,
+    price: 589879
+  },
+  {
+    title: 'm.7',
+    rooms: 2,
+    area: 43.7,
+    floor: 5,
+    parking: true,
+    price: 459789
+  },
+]
+
+export default Appartments
